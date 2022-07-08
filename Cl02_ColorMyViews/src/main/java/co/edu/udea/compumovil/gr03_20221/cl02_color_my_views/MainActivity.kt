57296a564636bfc12d991075fdd5e31f4e7dae18
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2022-07-07 21:51:37 -0500

    CodeLab02 Color My Views conf inicial
    Co-authored-by: Cristian Serna <mol.tired@gmail.com>
    Co-authored-by: Andres Grisales <andres.grisalesgg@gmail.com>

---
+++ Cl02_ColorMyViews/src/main/java/co/edu/udea/compumovil/gr03_20221/cl02_color_my_views/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.gr03_20221.cl02_color_my_views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
