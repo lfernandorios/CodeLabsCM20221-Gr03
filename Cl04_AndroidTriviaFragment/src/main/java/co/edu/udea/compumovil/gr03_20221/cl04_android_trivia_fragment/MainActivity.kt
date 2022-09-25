325f0d4071537b779b253919b546d88b66ab3e5e
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2022-09-25 18:14:52 -0500

    Init Codelab 04 Android Trivia Fragment
    Co-authored-by: Andres Grisales <andres.grisalesgg@gmail.com>

---
+++ Cl04_AndroidTriviaFragment/src/main/java/co/edu/udea/compumovil/gr03_20221/cl04_android_trivia_fragment/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.gr03_20221.cl04_android_trivia_fragment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ Cl04_AndroidTriviaFragment/src/test/java/co/edu/udea/compumovil/gr03_20221/cl04_android_trivia_fragment/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package co.edu.udea.compumovil.gr03_20221.cl04_android_trivia_fragment
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
