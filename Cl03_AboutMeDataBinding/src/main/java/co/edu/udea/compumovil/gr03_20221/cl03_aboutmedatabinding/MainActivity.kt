ab4013dd399010bdd2b938f008b28c908f24f5ee
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2022-09-25 16:20:25 -0500

    Codelab 03 init

---
+++ Cl03_AboutMeDataBinding/src/main/java/co/edu/udea/compumovil/gr03_20221/cl03_aboutmedatabinding/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.edu.udea.compumovil.gr03_20221.cl03_aboutmedatabinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
