da35472a6268260c14fff192fbff8ce08c394966
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2022-09-26 22:08:36 -0500

    Task: Add an Up button in the app bar
    Co-authored-by: Andres Grisales <andres.grisalesgg@gmail.com>

--- Cl04_AndroidTriviaFragment/src/main/java/co/edu/udea/compumovil/gr03_20221/cl04_android_trivia_fragment/MainActivity.kt
+++ Cl04_AndroidTriviaFragment/src/main/java/co/edu/udea/compumovil/gr03_20221/cl04_android_trivia_fragment/MainActivity.kt
@@ -3,6 +3,8 @@ package co.edu.udea.compumovil.gr03_20221.cl04_android_trivia_fragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import co.edu.udea.compumovil.gr03_20221.cl04_android_trivia_fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 
 
