d2ea3b221f0ceb764b0a9cd0ee5021e57b09c5ec
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2022-09-26 22:35:59 -0500

    Task: Add the navigation drawer
    Co-authored-by: Andres Grisales <andres.grisalesgg@gmail.com>

--- Cl04_AndroidTriviaFragment/src/main/java/co/edu/udea/compumovil/gr03_20221/cl04_android_trivia_fragment/MainActivity.kt
+++ Cl04_AndroidTriviaFragment/src/main/java/co/edu/udea/compumovil/gr03_20221/cl04_android_trivia_fragment/MainActivity.kt
@@ -3,23 +3,27 @@ package co.edu.udea.compumovil.gr03_20221.cl04_android_trivia_fragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import co.edu.udea.compumovil.gr03_20221.cl04_android_trivia_fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
-
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 
 
