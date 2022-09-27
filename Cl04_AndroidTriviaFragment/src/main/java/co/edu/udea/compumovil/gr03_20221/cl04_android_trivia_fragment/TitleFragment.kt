f093cafe8d6fbf33ca871f3065a2f619fa3aaa5e
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2022-09-26 22:16:52 -0500

    Task: Add an options menu
    Co-authored-by: Andres Grisales <andres.grisalesgg@gmail.com>

--- Cl04_AndroidTriviaFragment/src/main/java/co/edu/udea/compumovil/gr03_20221/cl04_android_trivia_fragment/TitleFragment.kt
+++ Cl04_AndroidTriviaFragment/src/main/java/co/edu/udea/compumovil/gr03_20221/cl04_android_trivia_fragment/TitleFragment.kt
@@ -1,12 +1,11 @@
 package co.edu.udea.compumovil.gr03_20221.cl04_android_trivia_fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import co.edu.udea.compumovil.gr03_20221.cl04_android_trivia_fragment.databinding.FragmentTitleBinding
 
 
@@ -24,6 +23,18 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener{ view : View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+            onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
